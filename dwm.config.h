e53e0d5ce35681e9472dc49f84a4d8d8d3e6a226
newnix <newnix@exile.digital>
2018-02-16 21:18:17 +0000

    Updated to new configuration
      * dwm.config.h
        - Updated configuration

--- dwm.config.h
+++ dwm.config.h
@@ -2,19 +2,24 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"Terminess Powerline:size=9"
+	"Termsynu:size=9"
 };
-static const char dmenufont[]       = "Terminess Powerline:size=9";
+static const char dmenufont[]       = "Termsynu:size=9";
 static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
-static const char selfgcolor[]      = "#eeeeee";
+static const char col_gray1[]				= "#222222";
+static const char col_gray2[]				= "#bbbbbb";
+static const char col_gray3[] 		  = "#eeeeee";
+static const char col_cyan[]      	= "#005577";
+static const char col_gray4[]      	= "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *colors[][3]			= {
+	/*	i						 fg					bg				 border			*/
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -54,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
